1a4b9e113835d60fb72ee9f8d81eca8eb11cef51
NicholasGin <nicholasgin@gmail.com>
2021-02-07 19:24:00 -0500

    added implementation for YearWheel

--- src/Question3/CalendarCounter.java
+++ src/Question3/CalendarCounter.java
@@ -2,7 +2,23 @@ package Question3;
 
 import Question1.CounterDisplay;
 
-public class CalendarCounter implements CounterDisplay {
+public class CalendarCounter extends YearWheel implements CounterDisplay {
+    YearWheel yearWheel = new YearWheel();
+    MonthWheel monthWheel = new MonthWheel();
+    DayWheel dayWheel = new DayWheel();
+    WeekDaysWheel weekDaysWheel = new WeekDaysWheel();
+
+    public CalendarCounter(int initialMonth, int initialDay, int initialYear){
+
+    }
+
+    public String toString(){
+        return "";
+    }
+
+
+
+
     @Override
     public void reset() {
 
--- src/Question3/YearWheel.java
+++ src/Question3/YearWheel.java
@@ -4,23 +4,29 @@ import Question1.Rollable;
 import Question2.Wheel;
 
 public class YearWheel extends Wheel<Integer> implements Rollable{
+    private int year;
+
+    public boolean isLeapYear(){
+        return year % 4 == 0;
+    }
     @Override
     public void rollUp() {
-
+        super.setValue(getValue()+1);
     }
 
     @Override
     public void rollDown() {
-
+        super.setValue(getValue()-1);
     }
 
     @Override
     public void reset() {
-
+        super.setValue(0);
     }
 
     @Override
     public Boolean isRolledOver() {
         return null;
     }
+
 }
